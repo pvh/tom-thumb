d9f27ef2684d7b1d12b6c320c6ddd674a5ec59e4
Peter van Hardenberg <pvh@heroku.com>
2010-06-09 03:09:11 -0400

    make rake jobs:work work and back it on mongo too, also add a perform that stores in the gridfs.

--- lib/thumbnail.rb
+++ lib/thumbnail.rb
@@ -11,5 +11,17 @@ class Thumbnail
     img.to_blob
   end
 
+  def perform
+    MongoConnection.gfs.open(@url, 'w', :delete_old => true) do |f|
+      f.write render
+    end
+  end
+
+  def read
+    MongoConnection.gfs.open(@url, 'r') do |f|
+      f.read
+    end
+  end
+
 end
 
--- lib/tom.rb
+++ lib/tom.rb
@@ -5,3 +5,30 @@ require 'uri'
 require 'open-uri'
 
 require 'lib/thumbnail'
+
+require 'delayed_job'
+require 'mongo_mapper'
+
+module MongoConnection
+  extend self
+  def connection
+    @conn ||= Mongo::Connection.from_uri(db_url)
+  end
+  def mongo
+    @mongo ||= connection.db(db_name)
+  end
+  def gfs
+    @gfs ||= Mongo::GridFileSystem.new(mongo)
+  end
+  def db_name
+    db_url.split('/').last
+  end
+  def db_url
+    ENV['MONGO_URL'] || 'mongodb://localhost:localhost@localhost/test'
+  end
+end
+
+MongoMapper.connection = MongoConnection.connection
+MongoMapper.database = MongoConnection.db_name
+Delayed::Worker.backend = :mongo_mapper
+
