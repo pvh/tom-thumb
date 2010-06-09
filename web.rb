1298e3b22b0318b4102012a4681f071823b56021
Peter van Hardenberg <pvh@heroku.com>
2010-06-09 12:20:30 -0400

    i so don't need jsonp

--- web.rb
+++ web.rb
@@ -18,6 +18,6 @@ end
 
 helpers do
   def photoset
-   JSON.parse( open("http://api.flickr.com/services/feeds/photoset.gne?set=72157624231740192&nsid=43850926@N08&lang=en-us&format=json&jsoncallback=?").read[1..-2] )
+   JSON.parse( open("http://api.flickr.com/services/feeds/photoset.gne?set=72157624231740192&nsid=43850926@N08&lang=en-us&format=json") )
   end
 end
